7cd123d77296fa4ca7ee6ddd6117b60d809e6dda
Prad Nukala <prad@sonr.io>
2023-12-14 20:57:09 +0000

    Dec 14, 2023, 3:57 PM

--- pkg/mpc/network/network_test.go
+++ pkg/mpc/network/network_test.go
@@ -19,3 +19,45 @@ func TestNewNetwork(t *testing.T) {
     assert.Nil(t, err)
     fmt.Println(sig)
 }
+func TestSign(t *testing.T) {
+  n := network.NewNetwork()
+
+  // Generate keys
+  err := n.Generate()
+  assert.Nil(t, err)
+
+  // Sign message
+  msg := []byte("test message")
+  sig, err := n.Sign(msg)
+  assert.Nil(t, err)
+  assert.NotNil(t, sig)
+
+  // Verify signature
+  valid, err := n.Verify(msg, sig)
+  assert.Nil(t, err)
+  assert.True(t, valid)
+}
+
+func TestVerify(t *testing.T) {
+  n := network.NewNetwork()
+
+  // Generate keys
+  err := n.Generate()
+  assert.Nil(t, err)
+
+  // Sign message
+  msg := []byte("test message 2")
+  sig, err := n.Sign(msg)
+  assert.Nil(t, err)
+
+  // Verify signature
+  valid, err := n.Verify(msg, sig)
+  assert.Nil(t, err)
+  assert.True(t, valid)
+
+  // Verify fails with different message
+  msg2 := []byte("different message")
+  valid, err = n.Verify(msg2, sig)
+  assert.Nil(t, err)
+  assert.False(t, valid)
+}
