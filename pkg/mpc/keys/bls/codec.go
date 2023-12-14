d61851d0893e788ca7f7596d6d975ace2ea967dd
Prad Nukala <prad@sonr.io>
2023-12-14 05:39:53 +0000

    Dec 14, 2023, 12:39 AM

--- pkg/mpc/keys/bls/codec.go
+++ pkg/mpc/keys/bls/codec.go
@@ -37,9 +37,48 @@ func (s *SecretKey) CreateAccumulator() (*Accumulator, error) {
 	return &Accumulator{Accumulator: acc, crv: s.crv}, nil
 }
 
+// OpenAccumulator opens an accumulator
+func (s *SecretKey) OpenAccumulator(acc []byte) (*Accumulator, error) {
+	e := new(accumulator.Accumulator)
+	err := e.UnmarshalBinary(acc)
+	if err != nil {
+		return nil, err
+	}
+	return &Accumulator{Accumulator: e, crv: s.crv}, nil
+}
+
 
 // Accumulator is the secret key for the BLS scheme
 type Accumulator struct {
 	*accumulator.Accumulator
 	crv *curves.PairingCurve
+}
+
+// AddValue adds a value to the accumulator
+func (a *Accumulator) AddValue(k *SecretKey, value string) error {
+	element := a.crv.Scalar.Hash([]byte(value))
+	elements := []accumulator.Element{element}
+	acc, _, err := a.Accumulator.Update(k.SecretKey, elements, nil)
+	if err != nil {
+		return err
+	}
+	a.Accumulator = acc
+	return nil
+}
+
+// RemoveValue removes a value from the accumulator
+func (a *Accumulator) RemoveValue(k *SecretKey, value string) error {
+	element := a.crv.Scalar.Hash([]byte(value))
+	elements := []accumulator.Element{element}
+	acc, _, err := a.Accumulator.Update(k.SecretKey, nil, elements)
+	if err != nil {
+		return err
+	}
+	a.Accumulator = acc
+	return nil
+}
+
+// Marshal marshals the accumulator
+func (a *Accumulator) Marshal() ([]byte, error) {
+	return a.Accumulator.MarshalBinary()
 }
